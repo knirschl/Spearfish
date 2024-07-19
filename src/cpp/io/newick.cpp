6b53b1b25770bd3426e48b9792a8f7e6bdb21e81
knirschl <lumokni@gmail.com>
2024-07-19 20:58:38 +0200

    Implement Newick Parser
    Currently just a validator as no ouptout except errors

---
+++ src/cpp/io/newick.cpp
@@ -0,0 +1,304 @@
+#include <exception>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+#include "src/io/newick.hpp"
+
+/**
+ * NEWICK LEXER
+ */
+
+spearfish_lexeme_t spearfish_newick_lexer_t::consume()
+{
+    auto token{peak()};
+    if (token == VALUE)
+    {
+      // we have a value, so we need to scan until we have found punctuation, or
+      // the end of the string
+      auto start_itr{_input.begin() + _current_index};
+      auto end_itr{start_itr};
+      while (char tmp = _input[_current_index])
+      {
+          if (is_punct(tmp))
+          {
+            break;
+          }
+          end_itr++;
+          _current_index++;
+      }
+
+      _value = std::string{start_itr, end_itr};
+      while (std::isspace(*(_value.end() - 1)))
+      {
+          _value.resize(_value.size() - 1);
+      }
+      return token;
+    } else
+    {
+        _current_index++;
+        skip_whitespace();
+        return token;
+    }
+}
+
+std::string spearfish_newick_lexer_t::consume_value_as_string()
+{
+    std::string tmp;
+    std::swap(tmp, _value);
+    return tmp;
+}
+
+double spearfish_newick_lexer_t::consume_value_as_double()
+{
+    auto d_str{consume_value_as_string()};
+    size_t pos{0};
+    double val{std::stod(d_str, &pos)};
+    if (pos != d_str.size())
+    {
+        throw std::runtime_error{std::string("Double conversion failed around")
+                                + describe_position()};
+    }
+    return val;
+}
+
+// TODO peak, is_punc very similar, do only one switch / if?
+spearfish_lexeme_t spearfish_newick_lexer_t::peak()
+{
+    size_t tmp_index{_current_index};
+    char current_char{_input[tmp_index++]};
+    if (is_punct(current_char))
+    {
+        switch (current_char)
+        {
+            case '[':
+                return OPENING_SQUARE_BRACKET;
+            case ']':
+                return CLOSING_SQUARE_BRACKET;
+            case '(':
+                return OPENING_PARENTHESIS;
+            case ')':
+                return CLOSING_PARENTHESIS;
+            case ':':
+                return COLON;
+            case ';':
+                return SEMICOLON;
+            case ',':
+                return COMMA;
+            case 0:
+            case EOF:
+                return END;
+            default:
+                throw std::runtime_error{"The punctuation was unrecognized"};
+        }
+    }
+    else
+    {
+        return VALUE;
+    }
+}
+
+void spearfish_newick_lexer_t::expect(spearfish_lexeme_t token_type)
+{
+    auto ret = consume_token_pos();
+    if (ret.first != token_type)
+    {
+      throw std::runtime_error{
+          std::string("Got the wrong token type at position ")
+          + std::to_string(ret.second + 1) + ". Was expecting "
+          + describe_token(token_type) + " but got "
+          + describe_token(ret.first)};
+    }
+}
+
+bool spearfish_newick_lexer_t::is_punct(char c)
+{
+    return c == '[' || c == ']' || c == '(' || c == ')' || c == ':' || c == ';'
+          || c == ',' || c == 0 || c == EOF;
+}
+
+std::pair<spearfish_lexeme_t, size_t> spearfish_newick_lexer_t::consume_token_pos()
+{
+    auto start_index{_current_index};
+    auto token{consume()};
+    return {token, start_index};
+}
+
+void spearfish_newick_lexer_t::skip_whitespace()
+{
+  while (_current_index < _input.size())
+  {
+    if (!std::isspace(_input[_current_index]))
+    {
+        break;
+    }
+    _current_index++;
+  }
+}
+
+std::string spearfish_newick_lexer_t::describe_token(spearfish_lexeme_t token_type)
+{
+  switch (token_type)
+  {
+    case OPENING_SQUARE_BRACKET:
+        return {"opening square bracket"};
+    case CLOSING_SQUARE_BRACKET:
+        return {"closing square bracket"};
+    case OPENING_PARENTHESIS:
+        return {"opening parenthesis"};
+    case CLOSING_PARENTHESIS:
+        return {"closing parenthesis"};
+    case COLON:
+        return {"colon"};
+    case SEMICOLON:
+        return {"semicolon"};
+    case COMMA:
+        return {"comma"};
+    case END:
+        return {"end of input"};
+    case VALUE:
+        return {"either a identifier or a number"};
+    default:
+        return {"unknown token"};
+  }
+}
+
+/**
+ * NEWICK PARSER
+ */
+
+void spearfish_newick_parser_t::parse_tree()
+{
+    try // TODO possible via return value?
+    {
+        parse_subtree();
+        if (_lexer.peak() != SEMICOLON)
+        {
+            parse_leaf();
+        } else
+        {
+            // TODO check obs in else korrekt ist oder ohne else immer ausgeführt werden sollte
+            // We overcounted, because we assume there is an "upper" branch still
+            _edge_count--;
+            // inner_count--;
+        }
+        _lexer.expect(SEMICOLON);
+        if (!_lexer.at_end())
+        {
+            throw std::runtime_error{
+                "There were extra characters after the tree was parsed"};
+        }
+    } catch (...)
+    {
+        // TODO free memory matrix, r, ...???
+        throw;
+    }
+}
+
+void spearfish_newick_parser_t::parse_subtree()
+{
+  _edge_count++;
+
+  if (_lexer.peak() == OPENING_PARENTHESIS)
+  {
+    parse_internal();
+  } else
+  {
+    parse_leaf();
+  }
+}
+
+void spearfish_newick_parser_t::parse_internal()
+{
+    _lexer.expect(OPENING_PARENTHESIS);
+    size_t start_leaf{_leaf_count}; // first leaf in subtree => start index in matrix
+    parse_node_set();
+    // TODO singleton subtree check (corax)???
+    
+    _lexer.expect(CLOSING_PARENTHESIS);
+    
+    auto length;
+    parse_node_attrs(length);
+    /* TODO
+    matrix.add(length, start_leaf, _leaf_count);
+    _r.add(length, start_leaf, _leaf_count);
+    */
+    
+    _inner_count++:
+}
+
+void spearfish_newick_parser_t::parse_node_set()
+{
+    parse_subtree();
+    if (_lexer.peak() == COMMA)
+    {
+        _lexer.consume();
+        parse_node_set();
+    }
+}
+
+void spearfish_newick_parser_t::parse_node_attrs(std::string& name, size_t& length)
+{
+    parse_name(name);
+    parse_comment();
+    parse_length(length);
+    parse_comment();
+}
+
+void spearfish_newick_parser_t::parse_leaf()
+{
+    std::string name;
+    size_t length;
+    parse_node_attrs(name, length);
+    if (name.empty()) // TODO correct check (nullptr/empty/...)
+    {
+        throw std::runtime_error{
+            std::string{"Found a leaf without a name around "}
+            + _lexer.describe_position()};
+    }
+    /* TODO
+    // _leaf_count == 0 => _r = ()
+    //       => push 0 or nothing depending on implementation of matrix
+    matrix.push_row(_r + length);
+    _r.push_back(length);
+    */ 
+    _leaf_count++;
+}
+
+void spearfish_newick_parser_t::parse_name(std::string& name)
+{
+    if (_lexer.peak() == VALUE)
+    {
+        _lexer.consume();
+        name = parse_string();
+    }
+}
+
+void spearfish_newick_parser_t::parse_length(size_t& length)
+{
+    if (_lexer.peak == COLON)
+    {
+        _lexer.consume();
+        _lexer.expect(VALUE);
+        length = parse_number();
+    }
+}
+
+void spearfish_newick_parser_t::parse_comment()
+{
+    if (_lexer.peak() == OPENING_SQUARE_BRACKET)
+    {
+        _lexer.consume();
+        _lexer.consume_until(CLOSING_SQUARE_BRACKET);
+    }
+}
+
+std::string spearfish_newick_parser_t::parse_string()
+{
+  return _lexer.consume_value_as_string();
+}
+
+double spearfish_newick_parser_t::parse_number()
+{
+  return _lexer.consume_value_as_double();
+}
--- src/cpp/io/newick.hpp
+++ src/cpp/io/newick.hpp
@@ -1,1 +1,363 @@
-// Implement coraxlib newick parser but make it build matrix directly
+/**
+ From coraxlib but for newick -> dm conversion
+ */
+
+#ifndef SPEARFISH_IO_NEWICK_HPP
+#define SPEARFISH_IO_NEWICK_HPP
+
+#include <fstream>
+
+/**
+ * Lexeme types for the newick parser.
+ */
+enum spearfish_lexeme_t
+{
+    OPENING_SQUARE_BRACKET,
+    CLOSING_SQUARE_BRACKET,
+    OPENING_PARENTHESIS,
+    CLOSING_PARENTHESIS,
+    COLON,
+    SEMICOLON,
+    COMMA,
+    VALUE,
+    END
+};
+
+/**
+ * A lexer for the newick parser. In general, users should not consume this
+ * directly, but instead use the `spearfish_newick_parser_t`.
+ */
+class spearfish_newick_lexer_t
+{
+public:
+    /**
+     * Initializes a lexer for the string `input` in newick format.
+     *
+     * @param input newick string
+     */
+    spearfish_newick_lexer_t(std::string input) :
+        _input{std::move(input)},
+        _current_index{0} {};
+
+    /**
+     * Consume next token.
+     *
+     * @return The consumed token
+     */
+    spearfish_lexeme_t consume();
+
+    /**
+     * Consume tokens until matching token (including). Acts like a discard.
+     *
+     * @param token_type Token until every other token should be discarded  
+     */
+    void consume_until(spearfish_lexeme_t token_type)
+    {
+        while (token_type != consume()) {}
+    }
+
+    /**
+     * Read the current value.
+     *
+     * @return A string representation of the value
+     */
+    std::string consume_value_as_string();
+
+    /**
+     * Read the current value and convert it to a double.
+     *
+     * @return A double representing the value
+     * @throws std::runtime_error if conversion fails
+     */
+    double consume_value_as_double();
+
+    /**
+     * Look ahead at next token.
+     *
+     * @return The token at the next position
+     */
+    spearfish_lexeme_t peak();
+
+    /**
+     * Checks if the current token is the same as `token_type`.
+     *
+     * @throws std::runtime_error if mismatch
+     */
+    void expect(spearfish_lexeme_t token_type);
+
+    /**
+     * Give the current position of the lexer with a string.
+     *
+     * @return A description of the lexer's position
+     */
+    std::string describe_position() const
+    {
+        std::stringstream builder;
+        builder << "position " << _current_index;
+        return builder.str();
+    }
+
+    /**
+     * Checks if end of string is reached.
+     *
+     * @return True if lexer reached end of string
+     */
+    bool at_end()
+    {
+        return _input.size() == _current_index;
+    }
+
+private:
+    /**
+     * Check if `c` is special token.
+     *
+     * @param c Character to check
+     * @return True if `c` is in "[]():;,\0" or end of file*/
+    bool is_punct(char c);
+
+    /**
+     * Same as `consume()` but return position as well.
+     *
+     *@return A pair of the read token and its position
+     */
+    std::pair<spearfish_lexeme_t, size_t> consume_token_pos();
+
+    /**
+     * Discard all (white-)space until next token
+     */
+    void skip_whitespace();
+
+    /**
+     * Give a textual description of the `token_type`.
+     *
+     * @param token_type The token to describe
+     * @return A description of the token
+     */
+    std::string describe_token(spearfish_lexeme_t token_type);
+
+    /* member variables */
+    std::string _input;
+    std::string _value;
+    size_t      _current_index;
+};
+
+/**
+ * Parser for a newick encoded tree.
+ *
+ * Using the following grammar:
+ * ```
+ * <tree> ::=
+ *     <subtree> ";"
+ * <subtree> ::=
+ *     <leaf> |
+ *     <internal>
+ * <internal> ::=
+ *     "(" <node_set> ")" <node_attrs>
+ * <node_set> ::=
+ *     <node> |
+ *     <node> "," <node_set>
+ * <node> ::=
+ *     <subtree> <node_attrs>
+ * <node_attrs> ::=
+ *     <name> <length> <comment>
+ * <leaf> ::=
+ *     <node_attrs>
+ * <length> ::=
+ *     ":" <number> |
+ *     <empty>
+ * <name> ::=
+ *     <string> |
+ *     <empty>
+ * <string> ::=
+ *     anything but punctuation
+ * <number> ::=
+ *     [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
+ * <comment> ::=
+ *     "[" .* "]" |
+ *     <empty>
+ * <empty> ::=
+ *     ""
+ * ```
+ */
+class spearfish_newick_parser_t
+{
+public:
+    /**
+     * Construct a newick parser over the given string.
+     *
+     * @param input Newick string to be parsed
+     */
+     spearfish_newick_parser_t(std::string input) :
+        _lexer{std::move(input)},
+        _leaf_count{0},
+        _inner_count{0},
+        _edge_count{0} {};
+
+    /**
+     * Parse the newick tree which was passed to the constructor.
+     */
+    void spearfish_newick_lexer_t::parse()
+    {
+        parse_tree();
+    }
+
+private:
+    /**
+     * Function corresponding to the rule
+     *
+     * ```
+     * <tree> ::=
+     *     <subtree> ";"
+     * ```
+     */
+    void parse_tree();
+
+    /**
+     * Function corresponding to the rule
+     *
+     * ```
+     * <subtree> ::=
+     *     <leaf> |
+     *     <internal>
+     * ```
+     */
+    void parse_subtree();
+
+    /**
+     * Function corresponding to the rule
+     *
+     * ```
+     * <internal> ::=
+     *     "(" <node_set> ")" <node_attrs>
+     *
+     * Allocates memory by creating a `corax_unode_t`
+     * ```
+     */
+    void parse_internal(); // creates node
+
+    /**
+     * Function corresponding to the rules
+     * ```
+     * <node_set> ::=
+     *     <node> |
+     *     <node> "," <node_set>
+     * <node> ::=
+     *     <subtree> <node_attrs>
+     * ```
+     */
+    void parse_node_set();
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <node_attrs> ::=
+     *     <name> <length> <comment>
+     * ```
+     */
+    void parse_node_attrs(std::string& name, size_t& length);
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <leaf> ::=
+     *     <node_attrs>
+     * ```
+     *
+     * Allocates memory by creating a `corax_unode_t`
+     */
+    void parse_leaf(); // creates node
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <name> ::=
+     *     <string> |
+     *     <empty>
+     * ```
+     */
+    void parse_name(std::string& name);
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <length> ::=
+     *     ":" <number> |
+     *     <empty>
+     * ```
+     */
+    void parse_length(size_t& length);
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <comment> ::=
+     *     "[" .* "]" |
+     *     <empty>
+     * ```
+     */
+    void parse_comment();
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <string> ::=
+     *     anything but punctuation
+     * ```
+     */
+    std::string parse_string();
+
+    /**
+     * Function corresponding to the rule
+     * ```
+     * <number> ::=
+     *     [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
+     * ```
+     */
+    double parse_number();
+
+    /* member variables */
+    corax_newick_lexer_t _lexer;
+    size_t               _leaf_count;
+    size_t               _inner_count;
+    size_t               _edge_count;
+};
+
+
+//
+// TODO build matrix & return it
+// TODO move code below somewhere else??
+// TODO newick.cpp + newick_matrix.cpp with inheritance 
+//
+
+/**
+ * Wrapper function for spearfish_newick_parser_t::parse()
+ */
+void tree_parse_newick_string(std::string newick_string)
+{
+    try
+    {
+        spearfish_newick_parser_t np{newick_string};
+        np.parse(auto_unroot, allow_rooted);
+    } catch (std::invalid_argument &e)
+    {
+        //corax_set_error(CORAX_ERROR_INVALID_TREE, e.what());
+        //nullptr;
+    } catch (std::runtime_error &e)
+    {
+        //corax_set_error(CORAX_ERROR_NEWICK_SYNTAX, e.what());
+        //return nullptr;
+    }
+}
+
+/**
+ * Wrapper function for spearfish_newick_parser_t::parse()
+ */
+void tree_parse_newick(const std::string filename)
+{
+    std::ifstream newick_file{filename};
+    std::string newick_string{(std::istreambuf_iterator<char>(newick_file)),
+                              (std::istreambuf_iterator<char>())};
+    return tree_parse_newick_string(newick_string);
+}
+
+#endif // SPEARFISH_IO_NEWICK_HPP
