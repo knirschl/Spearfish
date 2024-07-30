7bfaadd6df61211eac8db0755d9dc352c3baf012
knirschl <lukas.knirsch@h-its.org>
2024-07-30 17:02:24 +0200

    Set include paths for cluster

--- src/cpp/io/args.hpp
+++ src/cpp/io/args.hpp
@@ -2,8 +2,8 @@
 #define SPEARFISH_IO_ARGS_HPP
 
 //#include "/home/balin/Documents/Programming/libs/argparse/include/argparse/argparse.hpp"
-#include "/home/fili/Documents/Programming/libs/argparse/include/argparse/argparse.hpp"
-//#include "/hits/basement/cme/knirsch/github/argparse/include/argparse/argparse.hpp"
+//#include "/home/fili/Documents/Programming/libs/argparse/include/argparse/argparse.hpp"
+#include "/hits/basement/cme/knirsch/github/argparse/include/argparse/argparse.hpp"
 
 class spearfish_argument_parser_t {
 private:
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -17,8 +17,8 @@ static const std::string CORRECTION_IDENT{"S~G"};
 static const std::string MATRX_PHY_FILE{".matrix.phy"};
 static const std::string GTREE_NWK_FILE{".geneTree.newick"};
 static const std::string START_TREE_NWK_FILE{".startGeneTree.newick"};
-static const std::string MADROOT_BIN{//"/hits/basement/cme/knirsch/github/"
-                                     "/home/balin/Documents/Programming/HITS/Bioinformatics/"
+static const std::string MADROOT_BIN{"/hits/basement/cme/knirsch/github/"
+                                     //"/home/balin/Documents/Programming/HITS/Bioinformatics/"
                                      "MADroot/bin/madRoot-fix"};
 static const std::string STD_OUT_ERR_MOD{" 2>&1"};
 
