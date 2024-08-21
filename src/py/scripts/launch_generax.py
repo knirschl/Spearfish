0cc95a37e8eada4992927797d542aa79d7a47b0d
knirschl <lukas.knirsch@h-its.org>
2024-08-21 20:15:28 +0200

    Enable only pick

--- src/py/scripts/launch_generax.py
+++ src/py/scripts/launch_generax.py
@@ -93,18 +93,18 @@ def build_generax_families_file_eval(datadir, subst_model, output, tree_prefix="
             for tree in fam.get_gene_tree_list(datadir, family):
                 if (not tree.startswith(tree_prefix)):
                     continue
-                if ("startTree" in tree):
+                if ("startGeneTree" in tree):
                     continue
                 #scale = float(re.search(r'(\d+(?:\.\d+)?)S~G', tree)[1])
                 # if (scale < 1 or scale > 4.5):
                 #  continue
                 treefam = family + eval_results_delim + tree.replace(".geneTree.newick", "")
-                #if (os.path.isfile(
-                #        os.path.join(fam.get_run_dir(datadir, subst_model, "generax_eval_run"),
-                #                     "results", treefam, "stats.txt"))):
+                if (os.path.isfile(
+                        os.path.join(fam.get_run_dir(datadir, subst_model, "generax_eval_run"),
+                                     "results", treefam, "stats.txt"))):
                     # already evaluated
-                #    skip += 1
-                #    continue
+                    skip += 1
+                    continue
                 writer.write("- " + treefam + "\n")
                 writer.write("starting_gene_tree = ")
                 writer.write(os.path.join(
